2ec2746cc3762bbe5cf5b95a78d1b023da6532c4
BruneZManu <98150791+BruneZManu@users.noreply.github.com>
2022-02-18 10:13:49 +0100

    commit

--- GiocoIndovinaIlNumero/GiocoIndovinaIlNumero/Program.cs
+++ GiocoIndovinaIlNumero/GiocoIndovinaIlNumero/Program.cs
@@ -6,7 +6,34 @@ namespace GiocoIndovinaIlNumero
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+         Console.WriteLine("E'stato generato un numero casuale, tenta di indovinare qual è!");
+         Console.WriteLine("Il punteggio parte da 100000 e scende in base ai tentativi!");
+           
+           
+            Random N = new Random();
+            int num = N.Next(1,100);
+            int tent= 1;
+            int i = 0;
+            int punteggio = 100000;
+
+                do{
+                    Console.WriteLine("Scrivi un numero: ");
+                    i+=1;
+                    tent = Convert.ToInt32(Console.ReadLine());
+                    if (tent < num)
+                    {
+                        Console.WriteLine("Il numero è più alto");
+                        Console.WriteLine("");
+                    }
+                    else if(tent > num)
+                    {
+                        Console.WriteLine( "Il numero è più basso") ;
+                        Console.WriteLine("");
+                    }
+                    
+                 }while(tent != num);
+
+            Console.WriteLine($"Il punteggio è di {punteggio-i*1000} ");
         }
     }
 }
